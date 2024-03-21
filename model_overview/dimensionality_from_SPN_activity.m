ed5d81a3e6e9d593d56f7d4c23903bb37ee86f42
dirkbeck <88050967+dirkbeck@users.noreply.github.com>
2024-03-21 19:25:59 +0530

    added decorated SV profiles

--- model_overview/dimensionality_from_SPN_activity.m
+++ model_overview/dimensionality_from_SPN_activity.m
@@ -1,6 +1,8 @@
 clear; close all
 rng('default')
 
+%% first, link striosome activity to possible spaces
+
 n_features = 2;
 n_pcs = 2;
 n_obs = 100;
@@ -57,3 +59,68 @@ zlim([-5 5])
 legend(["DM cluster 1","DM cluster 2","DM cluster 3","DM cluster 4"])
 view(3)
 set(gcf,'Renderer','painters')
+
+%% next, link spaces to choice
+
+
+optionsxy = linspace(0,1,5);
+[optionsx, optionsy] = meshgrid(optionsxy,optionsxy);
+actions_0D = randi(3,5)-1;
+actions_1stdim = [2 2 2 2 0; 2 2 0 2 2; 0 0 0 0 0; 1 0 1 0 1; 1 1 0 1 1];
+actions_2nddim = [2 0 0 0 1; 2 0 0 1 1; 2 2 0 0 1; 2 0 0 0 1; 2 0 0 1 1];
+actions_2D = [0 2 2 2 2; 1 0 2 2 2; 1 0 0 0 2; 1 1 1 0 0; 1 1 1 1 1];
+
+actions_list = {actions_0D,actions_1stdim,actions_2nddim,actions_2D};
+titles = (["non-D","1st dim. in space","2nd dim. in space","2D space"]);
+
+for act=1:length(actions_list)
+
+    actions = actions_list{act};
+
+    optionsx_vec = optionsx(:); optionsy_vec = optionsy(:); ...
+        actions_vec = actions(:);
+    
+    unique_actions = unique(actions);
+    unique_actions = unique_actions(unique_actions~=0); % remove "no action"
+    n_actions = length(unique_actions);
+    action_values = zeros([size(actions),n_actions]);
+    for i = 1:n_actions
+        action_i = unique_actions(i);
+        mdl_i = fitlm([optionsx_vec,optionsy_vec],...
+            double(actions_vec==action_i));
+        for j = 1:size(actions,1)
+            for k = 1:size(actions,2)
+                action_values(j,k,i) = mdl_i.Coefficients.Estimate(1) + ...
+                    mdl_i.Coefficients.Estimate(2) * optionsx(j,k) + ...
+                    mdl_i.Coefficients.Estimate(3) * optionsy(j,k);
+            end
+        end
+    end
+    action_values = min(max(action_values,0),1);
+    
+    cmap = actioncmap();
+    symbols = ["x","d","o","*","v"];
+    figure; hold on
+    decision_making_map_plot(cmap,action_values,optionsxy,titles(act))
+    xlabel("dim. 1")
+    ylabel("dim. 2")
+    for i = 1:n_actions
+        scatter(optionsy(actions_vec==unique_actions(i)),...
+            optionsx(actions_vec==unique_actions(i)),200,...
+            cmap(i+1,:),symbols(i),'LineWidth',3);
+    end
+    % add decision boundary for the 1D+ cases
+    if act>1
+        for i=1:size(action_values,3)
+            for j=1:size(action_values,3)
+                if i > j
+                    contour(optionsxy,optionsxy,squeeze(action_values(:,:,i))'-...
+                        squeeze(action_values(:,:,j))',[0 0],'k','LineWidth',5)
+                end
+            end
+        end
+    end
+    hold off
+
+end
+
